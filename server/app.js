7895243c19418559617080cebf1f81e8601c0b67
zizpzn <zizpzn@gmail.com>
2022-08-16 11:39:29 +0700

    Connecting to Mongo DB

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+// app
 const app = express();
+
+// DB
+mongoose
+  .connect(process.env.MONGO_DB, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error: ", err));
 
 app.get("/", (req, res) => {
   res.send("Hello NodeJS");
 });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
