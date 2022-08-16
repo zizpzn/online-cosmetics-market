1be797a779752ccf7a37307cd7a8c2d2710d6b10
zizpzn <zizpzn@gmail.com>
2022-08-16 16:31:58 +0700

    Controllers

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello NodeJS !!!");
-});
+const { sayHello } = require("../controllers/user");
+
+router.get("/", sayHello);
 
 module.exports = router;
