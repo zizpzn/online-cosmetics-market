ff5471ebb3dd4724b2bbd696f0e54e98eccc406d
zizpzn <zizpzn@gmail.com>
2022-08-18 14:15:37 +0700

    User signup

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,16 @@
-exports.sayHello = (req, res) => {
-  res.json({ message: "Hello World" });
+const User = require("../models/user");
+
+exports.signup = (req, res) => {
+  console.log("req.body: ", req.body);
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error,
+      });
+    }
+    res.json({
+      user,
+    });
+  });
 };
