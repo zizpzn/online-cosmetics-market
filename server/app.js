2d4140a4eda05ef83aa9d7e1098775d3ac0ebdf3
zizpzn <zizpzn@gmail.com>
2022-08-18 16:06:50 +0700

    Using Postman

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 // import route
 const userRoutes = require("./routes/user");
@@ -13,6 +16,11 @@ mongoose
   .connect(process.env.MONGO_DB, {})
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB Error: ", err));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // routes middleware
 app.use("/api", userRoutes);
